740efb5241e11b58affcf948a32d7c7a0b0d2871
Dumindu Jayarathne <dumij58@gmail.com>
2023-12-15 18:25:56 +0530

    Add flask-sqlalchemy & flask-migrate to the projrct

--- streakz/__init__.py
+++ streakz/__init__.py
@@ -2,13 +2,16 @@ import os
 
 from flask import Flask
 
+from .models import db, migrate
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'streakz.sqlite'),
+        SECRET_KEY= "dev",
+
+        # configure the SQLite database, relative to the app instance folder
+        SQLALCHEMY_DATABASE_URI = "sqlite:///streakz.db",
     )
 
     if test_config is None:
@@ -24,6 +27,15 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # initialize the app with flask-sqlalchemy and flask-migrate
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # create models defined in models.py
+    with app.app_context():
+        db.create_all()
+
+    # import and register blueprints
     from . import index
     app.register_blueprint(index.bp)
 
---
+++ streakz/models.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+#from sqlalchemy import Integer, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import List
+from datetime import datetime
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+migrate = Migrate()
+
+class Habit(db.Model):
+  id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+  title: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
+  desc: Mapped[str] = mapped_column(db.String)
+  entries: Mapped[List["Entry"]] = relationship(
+    back_populates="habit",
+    cascade="all, delete-orphan"
+  )
+  
+  def __repr__(self) -> str:
+    return f"Habit(id={self.id!r}, title={self.title!r})"
+
+
+class Entry(db.Model):
+  id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+  habit: Mapped["Habit"] = relationship(back_populates="entry")
+  checked_dt: Mapped[datetime] = mapped_column(db.DateTime, nullable=False)
+
+  def __repr__(self) -> str:
+    return f"Entry(id={self.id!r}, habit={self.habit!r})"
